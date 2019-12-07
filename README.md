f7b6a97b033794dd55a6ce773a1470ad14713a4d
Leandro Simões <leandro.simoes@outlook.com>
2019-12-07 00:13:06 -0300

    Add two more options language_in for the version of ecmascript of input files language_out for the version of ecmascript of output files Default is ECMASCRIPT_2018 for IN and ECMASCRIPT5 for out

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # ls-minifier
+
 A tool that minifies all .js and css files in a folder recursivelly using https://www.npmjs.com/package/node-minify
 
 ### Installation
@@ -7,41 +8,55 @@ A tool that minifies all .js and css files in a folder recursivelly using https:
 
 ### Run CLI
 
-    > ls-minifier --input=./your/files/path --js-compressor=gcc --css-compressor=yui --silent
+    > ls-minifier --input=./your/files/path --js-compressor=gcc --language-in=ECMASCRIPT_2018 --language-out=ECMASCRIPT5 --css-compressor=yui --silent
 
 ### Run in your code
+
 `lsMinifier` function has 3 parts: input-path, options, and callback, such that
 
     lsMinifier([input-path], [options], [callback])
 
 The callback outputs 2 options:
-* **err**: the error of each file
-* **min**: the output of the minified file
+
+-   **err**: the error of each file
+-   **min**: the output of the minified file
 
 ##### Example
 
 ```javascript
-const lsMinifier = require("ls-minifier")
+const lsMinifier = require('ls-minifier')
 const input_path = './'
-const options = { silent: true, js_compressor: 'gcc', css_compressor: 'yui' }
-const callback = (err, min) => err ? console.log(err) : console.log(min)
+const options = {
+    silent: true,
+    js_compressor: 'gcc',
+    css_compressor: 'yui',
+    language_in: 'ECMASCRIPT_2018',
+    language_out: 'ECMASCRIPT5',
+}
+const callback = (err, min) => (err ? console.log(err) : console.log(min))
 
 lsMinifier(input_path, options, callback)
 ```
 
 ### Options
 
-* **input**
-The input path for the files. (Default is the current console path)
+-   **input**
+    The input path for the files. (Default is the current console path)
 
-* **silent**
-If silent mode is on, then logs of which files has been found won't be displayed
+-   **silent**
+    If silent mode is on, then logs of which files has been found won't be displayed
 
-* **js_compressor**
-Determines the compression type that js files should be put through. (Default is yui)
+-   **js_compressor**
+    Determines the compression type that js files should be put through. (Default is yui)
 
-* **css_compressor**
-Determines the compression type that css files should be put through. (Default is yui)
+-   **language_in** (ONLY FOR GCC COMPRESSOR)
+    Determine the version of ecmascript of the input file. (Default is ECMASCRIPT_2018)
 
-PS: As **ls-minifier** depends on **node-minify**, these types are defined by **node-minify** and 
+-   **language_out** (ONLY FOR GCC COMPRESSOR)
+    Determine the version of ecmascript of the input file. (Default is ECMASCRIPT5)
+
+-   **css_compressor**
+    Determines the compression type that css files should be put through. (Default is yui)
+
+PS: As **ls-minifier** depends on **node-minify**, these types are defined by **node-minify** and
 can be found [here](https://www.npmjs.com/package/node-minify).
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const fs = require('fs')
 const path = require('path')
 const _compressor = require('node-minify')
 const DEFAULT_CALLBACK = err => err && console.log(err)
+const DEFAULT_LANGUAGE_IN = 'ECMASCRIPT_2018'
+const DEFAULT_LANGUAGE_OUT = 'ECMASCRIPT5'
 
 const delay = (miliseconds = 100) => {
     return new Promise(resolve => {
@@ -35,16 +37,14 @@ const walk = async (currentDirPath, callback) => {
     })
 }
 
-const lsMinifier = async (dir, options = {}, callback = DEFAULT_CALLBACK) => {
+const lsMinifier = async (dir, options = {}, callback = DEFAULT_CALLBACK, language_in = DEFAULT_LANGUAGE_IN, language_out = DEFAULT_LANGUAGE_OUT) => {
     const { js_compressor, css_compressor, silent = false } = options
 
     return await walk(dir, path => {
         return new Promise(resolve => {
-            const is_valid_js =
-                path.endsWith('.js') && !path.endsWith('.min.js')
+            const is_valid_js = path.endsWith('.js') && !path.endsWith('.min.js')
 
-            const is_valid_css =
-                path.endsWith('.css') && !path.endsWith('.min.css')
+            const is_valid_css = path.endsWith('.css') && !path.endsWith('.min.css')
 
             if (!is_valid_css && !is_valid_js) {
                 resolve()
@@ -52,10 +52,9 @@ const lsMinifier = async (dir, options = {}, callback = DEFAULT_CALLBACK) => {
             }
 
             const input = path
-            const output = path.replace(
-                is_valid_js ? '.js' : '.css',
-                is_valid_js ? '.min.js' : '.min.css'
-            )
+            const output = path.replace(is_valid_js ? '.js' : '.css', is_valid_js ? '.min.js' : '.min.css')
+
+            const compressor_options = is_valid_js && js_compressor === 'gcc' ? { languageIn: language_in, languageOut: language_out } : {}
 
             if (!silent) console.log(`Minifying ${path}`)
 
@@ -63,6 +62,7 @@ const lsMinifier = async (dir, options = {}, callback = DEFAULT_CALLBACK) => {
                 compressor: is_valid_js ? js_compressor : css_compressor,
                 input,
                 output,
+                options: compressor_options,
                 callback: (err, min) => {
                     callback(err, min)
                     resolve()
@@ -78,10 +78,12 @@ if (require.main === module) {
     const silent = !!(args.find(a => a.startsWith('--silent')) || '')
     const js_compressor = (args.find(a => a.startsWith('--js-compressor=')) || '').replace('--js-compressor=', '') || 'yui'
     const css_compressor = (args.find(a => a.startsWith('--css-compressor=')) || '').replace('--css-compressor=', '') || 'yui'
+    const language_in = (args.find(a => a.startsWith('--language-in=')) || '').replace('--language-in=', '') || ''
+    const language_out = (args.find(a => a.startsWith('--language-out=')) || '').replace('--language-out=', '') || ''
 
     if (!input) throw new Error('Input Path is required')
 
-    lsMinifier(input, { js_compressor, css_compressor, silent })
+    lsMinifier(input, { js_compressor, css_compressor, silent, language_in, language_out })
         .then(() => {
             console.log('Minification finished')
         })

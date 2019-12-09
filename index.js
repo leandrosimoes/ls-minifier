021fc2288733db8d9eca26ca42eed5bf9af9e356
Leandro Simões <leandro.simoes@outlook.com>
2019-12-09 12:42:28 -0300

    Add option to set a signature file that will contains come text that you want to show at the top of the minified files

--- index.js
+++ index.js
@@ -5,6 +5,37 @@ const _compressor = require('node-minify')
 const DEFAULT_CALLBACK = err => err && console.log(err)
 const DEFAULT_LANGUAGE_IN = 'ECMASCRIPT_2018'
 const DEFAULT_LANGUAGE_OUT = 'ECMASCRIPT5'
+let SIGNATURE = ''
+
+const signFile = (path, signature_file_path) => {
+    const is_valid_js = path.endsWith('.js')
+    const is_valid_css = path.endsWith('.css')
+    const is_valid_html = (path.endsWith('.html') || path.endsWith('.htm'))
+
+    if (!is_valid_js && !is_valid_css && !is_valid_html) return;
+    
+    let init = ''
+    let end = ''
+
+    if (is_valid_js || is_valid_css) {
+        init = '/*'
+        end = '*/'
+    } else if (is_valid_html) {
+        init = '<!--'
+        end = '-->'
+    }
+
+    SIGNATURE = SIGNATURE || fs.readFileSync(signature_file_path)
+
+    const output_file_content = fs.readFileSync(path)
+    const signed_file_content = `
+${init}
+${SIGNATURE}
+${end}
+${output_file_content}`
+
+    fs.writeFileSync(path, signed_file_content)
+}
 
 const delay = (miliseconds = 100) => {
     return new Promise(resolve => {
@@ -45,7 +76,8 @@ const lsMinifier = async (dir, options = {}, callback = DEFAULT_CALLBACK) => {
         silent = false, 
         js_language_in = DEFAULT_LANGUAGE_IN, 
         js_language_out = DEFAULT_LANGUAGE_OUT, 
-        override = false 
+        override = false,
+        signature_file
     } = options
 
     return await walk(dir, path => {
@@ -84,6 +116,8 @@ const lsMinifier = async (dir, options = {}, callback = DEFAULT_CALLBACK) => {
                 output,
                 options: compressor_options,
                 callback: (err, min) => {
+                    if (!!signature_file) signFile(output, signature_file)
+
                     callback(err, min)
                     resolve()
                 },
@@ -102,10 +136,11 @@ if (require.main === module) {
     const js_language_in = (args.find(a => a.startsWith('--language-in=')) || '').replace('--language-in=', '') || DEFAULT_LANGUAGE_IN
     const js_language_out = (args.find(a => a.startsWith('--language-out=')) || '').replace('--language-out=', '') || DEFAULT_LANGUAGE_OUT
     const override = !!(args.find(a => a.startsWith('--override')) || '')
+    const signature_file = (args.find(a => a.startsWith('--signature-file=')) || '').replace('--signature-file=', '') || ''
 
     if (!input) throw new Error('Input Path is required')
 
-    lsMinifier(input, { js_compressor, css_compressor, html_compressor, silent, js_language_in, js_language_out, override })
+    lsMinifier(input, { js_compressor, css_compressor, html_compressor, silent, js_language_in, js_language_out, override, signature_file })
         .then(() => {
             console.log('Minification finished')
         })
